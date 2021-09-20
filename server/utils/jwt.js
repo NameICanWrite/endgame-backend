4fad0ab9d85fcffef5af02bbd7043dcb3886e937
Vadim <vadimbaranivsky83@gmail.com>
2021-09-20 12:24:44 +0300

    -

--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -10,10 +10,10 @@ import {promisify} from 'util'
 
     const cookieOptions= {
       expires: new Date(Date.now()+process.env.JWT_COOKIES_EXPIRES_IN*24*3600*1000),
-      httpOnly: false,
+      httpOnly: true,
     }
   
-    // if(process.env.NODE_ENV=== 'production') cookieOptions.secure = true
+    if(process.env.NODE_ENV=== 'production') cookieOptions.secure = true
   
     res.cookie('jwt', token, cookieOptions);
   }
